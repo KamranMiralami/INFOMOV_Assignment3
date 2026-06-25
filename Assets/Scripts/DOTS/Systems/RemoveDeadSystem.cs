727f2bb6c9e9aeac8578c08f77e49602f31be873
Meraki3s4d <106313377+Meraki3s4d@users.noreply.github.com>
2026-06-25 20:12:59 +0200

    Solve "RemoveDeadSystem" cause performance issue

--- Assets/Scripts/DOTS/Systems/RemoveDeadSystem.cs
+++ Assets/Scripts/DOTS/Systems/RemoveDeadSystem.cs
@@ -9,21 +9,35 @@ partial struct RemoveDeadSystem : ISystem
 	public void OnCreate(ref SystemState state)
 	{
 		state.RequireForUpdate<Health>();
+		state.RequireForUpdate<EndSimulationEntityCommandBufferSystem.Singleton>();
 	}
 
 	[BurstCompile]
 	public void OnUpdate(ref SystemState state)
 	{
-		using (var commandBuffer = new EntityCommandBuffer(Allocator.TempJob))
+		var ecbSingleton = SystemAPI.GetSingleton<EndSimulationEntityCommandBufferSystem.Singleton>();
+		var ecb = ecbSingleton.CreateCommandBuffer(state.WorldUnmanaged);
+		
+		var removeDeadJob = new RemoveDeadJob
 		{
-			foreach (var (health, entity) in SystemAPI.Query<RefRO<Health>>().WithAll<EnemyTag>().WithEntityAccess())
-			{
-				if (health.ValueRO.Value <= 0f)
-				{
-					commandBuffer.DestroyEntity(entity);
-				}
-			}
-			commandBuffer.Playback(state.EntityManager);
+			ecbParallelWriter = ecb.AsParallelWriter()
+		};
+		
+		state.Dependency = removeDeadJob.ScheduleParallel(state.Dependency);
+	}
+}
+
+[BurstCompile]
+[WithAll(typeof(EnemyTag))]
+partial struct RemoveDeadJob : IJobEntity
+{
+	public EntityCommandBuffer.ParallelWriter ecbParallelWriter;
+	
+	void Execute([ChunkIndexInQuery] int chunkIndex, Entity entity, in Health health)
+	{
+		if (health.Value <= 0f)
+		{
+			ecbParallelWriter.DestroyEntity(chunkIndex, entity);
 		}
 	}
 }
