756538442056b43b6126f4609e82ef73b8b49e3f
Kamran Miralami <kamran.m1380@yahoo.com>
2026-06-26 17:30:08 +0200

    added simd and fixed points (not much improvement however)

--- Assets/Scripts/DOTS/Systems/CollisionSystem.cs
+++ Assets/Scripts/DOTS/Systems/CollisionSystem.cs
@@ -28,42 +28,244 @@ partial struct CollisionSystem : ISystem
 	}
     [BurstCompile(FloatPrecision = FloatPrecision.Medium, FloatMode = FloatMode.Fast)]
     public void OnUpdate(ref SystemState state)
-	{
-        var jobEvB = new CollisionJob()
-		{
-			radiusSqr = enemyCollisionRadiusSqr,
-			transToTestAgainst = bulletQuery.ToComponentDataArray<LocalTransform>(state.WorldUpdateAllocator)
-			
-		};
-		state.Dependency = jobEvB.ScheduleParallel(enemyQuery, state.Dependency);
-		var jobPvE = new CollisionJob()
-		{
-			radiusSqr = playerCollisionRadiusSqr,
-			transToTestAgainst = enemyQuery.ToComponentDataArray<LocalTransform>(state.WorldUpdateAllocator)
-		};
-		state.Dependency = jobPvE.ScheduleParallel(playerQuery, state.Dependency);
-	}
+    {
+        bool useSIMD = true;
+        bool useFixedPoint = false;
+
+        if (useFixedPoint)
+        {
+            var rawBullets = bulletQuery.ToComponentDataArray<LocalTransform>(state.WorldUpdateAllocator);
+            var rawEnemies = enemyQuery.ToComponentDataArray<LocalTransform>(state.WorldUpdateAllocator);
+            var fpBullets = CollectionHelper.CreateNativeArray<fp2>(rawBullets.Length, state.WorldUpdateAllocator);
+            var fpEnemies = CollectionHelper.CreateNativeArray<fp2>(rawEnemies.Length, state.WorldUpdateAllocator);
+
+            // float to Q16.16
+            var convertBullets = new ConvertToFixedPointJob { Source = rawBullets, Destination = fpBullets };
+            var convertEnemies = new ConvertToFixedPointJob { Source = rawEnemies, Destination = fpEnemies };
+
+            JobHandle packingHandle = JobHandle.CombineDependencies(convertBullets.Schedule(), convertEnemies.Schedule());
+            JobHandle combinedDependency = JobHandle.CombineDependencies(state.Dependency, packingHandle);
+            var fpJobEvB = new FixedPointCollisionJob
+            {
+                radiusSqrRaw = fp.FromFloat(enemyCollisionRadiusSqr).raw,
+                transToTestAgainst = fpBullets
+            };
+            state.Dependency = fpJobEvB.ScheduleParallel(enemyQuery, combinedDependency);
+
+            var fpJobPvE = new FixedPointCollisionJob
+            {
+                radiusSqrRaw = fp.FromFloat(playerCollisionRadiusSqr).raw,
+                transToTestAgainst = fpEnemies
+            };
+            state.Dependency = fpJobPvE.ScheduleParallel(playerQuery, state.Dependency);
+        }
+        else if (useSIMD)
+        {
+            var rawBullets = bulletQuery.ToComponentDataArray<LocalTransform>(state.WorldUpdateAllocator);
+            var rawEnemies = enemyQuery.ToComponentDataArray<LocalTransform>(state.WorldUpdateAllocator);
+            int bulletCount = rawBullets.Length;
+            int bulletChunks = bulletCount / 4;
+            int bulletRem = bulletCount % 4;
+            var packedBulletX = CollectionHelper.CreateNativeArray<float4>(bulletChunks, state.WorldUpdateAllocator);
+            var packedBulletZ = CollectionHelper.CreateNativeArray<float4>(bulletChunks, state.WorldUpdateAllocator);
+            var remBullets = CollectionHelper.CreateNativeArray<float2>(bulletRem, state.WorldUpdateAllocator);
+            int enemyCount = rawEnemies.Length;
+            int enemyChunks = enemyCount / 4;
+            int enemyRem = enemyCount % 4;
+            var packedEnemyX = CollectionHelper.CreateNativeArray<float4>(enemyChunks, state.WorldUpdateAllocator);
+            var packedEnemyZ = CollectionHelper.CreateNativeArray<float4>(enemyChunks, state.WorldUpdateAllocator);
+            var remEnemies = CollectionHelper.CreateNativeArray<float2>(enemyRem, state.WorldUpdateAllocator);
+
+            // AOS to SOA Conversion
+            var packBullets = new PackPositionsJob { Source = rawBullets, PackedX = packedBulletX, PackedZ = packedBulletZ, Remainder = remBullets };
+            var packEnemies = new PackPositionsJob { Source = rawEnemies, PackedX = packedEnemyX, PackedZ = packedEnemyZ, Remainder = remEnemies };
+            JobHandle packHandle = JobHandle.CombineDependencies(packBullets.Schedule(), packEnemies.Schedule());
+            JobHandle combinedDependency = JobHandle.CombineDependencies(state.Dependency, packHandle);
+
+            var simdEvB = new SIMDCollisionJob
+            {
+                radiusSqr = enemyCollisionRadiusSqr,
+                packedX = packedBulletX,
+                packedZ = packedBulletZ,
+                remainder = remBullets
+            };
+            state.Dependency = simdEvB.ScheduleParallel(enemyQuery, combinedDependency);
+
+            var simdPvE = new SIMDCollisionJob
+            {
+                radiusSqr = playerCollisionRadiusSqr,
+                packedX = packedEnemyX,
+                packedZ = packedEnemyZ,
+                remainder = remEnemies
+            };
+            state.Dependency = simdPvE.ScheduleParallel(playerQuery, state.Dependency);
+        }
+        else
+        {
+            var jobEvB = new CollisionJob()
+            {
+                radiusSqr = enemyCollisionRadiusSqr,
+                transToTestAgainst = bulletQuery.ToComponentDataArray<LocalTransform>(state.WorldUpdateAllocator)
+            };
+            state.Dependency = jobEvB.ScheduleParallel(enemyQuery, state.Dependency);
+            var jobPvE = new CollisionJob()
+            {
+                radiusSqr = playerCollisionRadiusSqr,
+                transToTestAgainst = enemyQuery.ToComponentDataArray<LocalTransform>(state.WorldUpdateAllocator)
+            };
+            state.Dependency = jobPvE.ScheduleParallel(playerQuery, state.Dependency);
+        }
+    }
+}
+public struct fp
+{
+    public int raw;
+    public const int SHIFT = 16;
+    public const int ONE = 1 << SHIFT;
+    public static fp FromFloat(float v) => new fp { raw = (int)(v * ONE) };
+}
+
+public struct fp2
+{
+    public int xRaw;
+    public int zRaw;
+
+    public static fp2 FromFloat3(float3 v)
+    {
+        return new fp2
+        {
+            xRaw = (int)(v.x * fp.ONE),
+            zRaw = (int)(v.z * fp.ONE)
+        };
+    }
 }
 
 [BurstCompile(FloatPrecision = FloatPrecision.Medium, FloatMode = FloatMode.Fast)]
-partial struct CollisionJob : IJobEntity 
+struct ConvertToFixedPointJob : IJob
 {
-	public float radiusSqr;
-	[ReadOnly] public NativeArray<LocalTransform> transToTestAgainst;
-	public void Execute(ref Health health, in LocalTransform transform)
-	{
-		float damage = 0f;
-		
-		float3 posA = transform.Position;
-		for (int i = 0; i < transToTestAgainst.Length; i++)
-		{
-			float3 posB = transToTestAgainst[i].Position;
-			
-			float distanceSquare = math.distancesq(posA.xz, posB.xz);
-			
-			damage += math.select(0f, 1f, distanceSquare <= radiusSqr);
-		}
-		if (damage > 0)
-			health.Value -= damage;
-	}
+    [ReadOnly] public NativeArray<LocalTransform> Source;
+    [WriteOnly] public NativeArray<fp2> Destination;
+
+    public void Execute()
+    {
+        for (int i = 0; i < Source.Length; i++)
+        {
+            Destination[i] = fp2.FromFloat3(Source[i].Position);
+        }
+    }
+}
+
+[BurstCompile(FloatPrecision = FloatPrecision.Medium, FloatMode = FloatMode.Fast)]
+partial struct FixedPointCollisionJob : IJobEntity
+{
+    public int radiusSqrRaw;
+    [ReadOnly] public NativeArray<fp2> transToTestAgainst;
+
+    public void Execute(ref Health health, in LocalTransform transform)
+    {
+        float damage = 0f;
+        int posAx = (int)(transform.Position.x * fp.ONE);
+        int posAz = (int)(transform.Position.z * fp.ONE);
+
+        for (int i = 0; i < transToTestAgainst.Length; i++)
+        {
+            long deltaX = posAx - transToTestAgainst[i].xRaw;
+            long deltaZ = posAz - transToTestAgainst[i].zRaw;
+
+            long sqrX = (deltaX * deltaX) >> fp.SHIFT;
+            long sqrZ = (deltaZ * deltaZ) >> fp.SHIFT;
+            long distanceSquareRaw = sqrX + sqrZ;
+            damage += math.select(0f, 1f, distanceSquareRaw <= radiusSqrRaw);
+        }
+
+        if (damage > 0f)
+        {
+            health.Value -= damage;
+        }
+    }
+}
+
+[BurstCompile(FloatPrecision = FloatPrecision.Medium, FloatMode = FloatMode.Fast)]
+struct PackPositionsJob : IJob
+{
+    // all these read-only and write-only attributes are to avoid race conditions and to inform the job system about the intended usage of these arrays.
+    [ReadOnly] public NativeArray<LocalTransform> Source;
+    [WriteOnly] public NativeArray<float4> PackedX;
+    [WriteOnly] public NativeArray<float4> PackedZ;
+    [WriteOnly] public NativeArray<float2> Remainder;
+    public void Execute()
+    {
+        int chunks = Source.Length / 4;
+        for (int i = 0; i < chunks; i++)
+        {
+            int idx = i * 4;
+            PackedX[i] = new float4(Source[idx].Position.x, Source[idx + 1].Position.x, Source[idx + 2].Position.x, Source[idx + 3].Position.x);
+            PackedZ[i] = new float4(Source[idx].Position.z, Source[idx + 1].Position.z, Source[idx + 2].Position.z, Source[idx + 3].Position.z);
+        }
+
+        int remStart = chunks * 4;
+        int remCount = Source.Length % 4;
+        for (int i = 0; i < remCount; i++)
+        {
+            Remainder[i] = Source[remStart + i].Position.xz;
+        }
+    }
+}
+
+[BurstCompile(FloatPrecision = FloatPrecision.Medium, FloatMode = FloatMode.Fast)]
+partial struct SIMDCollisionJob : IJobEntity
+{
+    public float radiusSqr;
+    [ReadOnly] public NativeArray<float4> packedX;
+    [ReadOnly] public NativeArray<float4> packedZ;
+    [ReadOnly] public NativeArray<float2> remainder;
+
+    public void Execute(ref Health health, in LocalTransform transform)
+    {
+        float damage = 0f;
+        float4 posAx = transform.Position.x;
+        float4 posAz = transform.Position.z;
+        float4 rSqr4 = radiusSqr;
+        for (int i = 0; i < packedX.Length; i++)
+        {
+            float4 deltaX = posAx - packedX[i];
+            float4 deltaZ = posAz - packedZ[i];
+            float4 distSq = (deltaX * deltaX) + (deltaZ * deltaZ);
+            damage += math.csum(math.select(float4.zero, new float4(1f), distSq <= rSqr4));
+        }
+        for (int i = 0; i < remainder.Length; i++)
+        {
+            float2 delta = transform.Position.xz - remainder[i];
+            float distSq = (delta.x * delta.x) + (delta.y * delta.y);
+            damage += math.select(0f, 1f, distSq <= radiusSqr);
+        }
+
+        if (damage > 0f)
+        {
+            health.Value -= damage;
+        }
+    }
+}
+
+[BurstCompile(FloatPrecision = FloatPrecision.Medium, FloatMode = FloatMode.Fast)]
+partial struct CollisionJob : IJobEntity
+{
+    public float radiusSqr;
+    [ReadOnly] public NativeArray<LocalTransform> transToTestAgainst;
+    public void Execute(ref Health health, in LocalTransform transform)
+    {
+        float damage = 0f;
+
+        float3 posA = transform.Position;
+        for (int i = 0; i < transToTestAgainst.Length; i++)
+        {
+            float3 posB = transToTestAgainst[i].Position;
+
+            float distanceSquare = math.distancesq(posA.xz, posB.xz);
+
+            damage += math.select(0f, 1f, distanceSquare <= radiusSqr);
+        }
+        if (damage > 0)
+            health.Value -= damage;
+    }
 }
--- Assets/Scripts/DOTS/Systems/MoveForwardSystem.cs
+++ Assets/Scripts/DOTS/Systems/MoveForwardSystem.cs
@@ -1,4 +1,5 @@
 ﻿using Unity.Burst;
+using Unity.Collections;
 using Unity.Entities;
 using Unity.Jobs;
 using Unity.Mathematics;
@@ -7,20 +8,125 @@ using Unity.Transforms;
 [BurstCompile(FloatPrecision = FloatPrecision.Medium, FloatMode = FloatMode.Fast)]
 partial struct MoveForwardSystem : ISystem
 {
+    EntityQuery moveQuery;
     [BurstCompile(FloatPrecision = FloatPrecision.Medium, FloatMode = FloatMode.Fast)]
     public void OnCreate(ref SystemState state)
     {
         state.RequireForUpdate<MoveSpeed>();
+        moveQuery = SystemAPI.QueryBuilder().WithAll<MoveForward, MoveSpeed, LocalTransform>().Build();
     }
 
     [BurstCompile(FloatPrecision = FloatPrecision.Medium, FloatMode = FloatMode.Fast)]
     public void OnUpdate(ref SystemState state)
     {
-        var MoveForwardJob = new MoveForwardJob
-		{
-            dt = SystemAPI.Time.DeltaTime
-        };
-        MoveForwardJob.ScheduleParallel();
+        bool useSIMDMovement = true;
+        bool useFixedPointMovement = false;
+
+        if (useFixedPointMovement)
+        {
+            var fixedPointMoveJob = new FixedPointMovementJob
+            {
+                dtRaw = (int)(SystemAPI.Time.DeltaTime * 65536f)
+            };
+            state.Dependency = fixedPointMoveJob.ScheduleParallel(moveQuery, state.Dependency);
+        }
+        else if (useSIMDMovement)
+        {
+            var simdChunkJob = new SIMDMovementChunkJob
+            {
+                dt = SystemAPI.Time.DeltaTime,
+                TransformHandle = SystemAPI.GetComponentTypeHandle<LocalTransform>(false),
+                SpeedHandle = SystemAPI.GetComponentTypeHandle<MoveSpeed>(true)
+            };
+            state.Dependency = simdChunkJob.ScheduleParallel(moveQuery, state.Dependency);
+        }
+        else
+        {
+            var MoveForwardJob = new MoveForwardJob
+            {
+                dt = SystemAPI.Time.DeltaTime
+            };
+            state.Dependency = MoveForwardJob.ScheduleParallel(moveQuery, state.Dependency);
+        }
+    }
+}
+
+[BurstCompile(FloatPrecision = FloatPrecision.Medium, FloatMode = FloatMode.Fast)]
+struct SIMDMovementChunkJob : IJobChunk
+{
+    public float dt;
+    public ComponentTypeHandle<LocalTransform> TransformHandle;
+    [ReadOnly] public ComponentTypeHandle<MoveSpeed> SpeedHandle;
+
+    public void Execute(in ArchetypeChunk chunk, int unfilteredChunkIndex, bool useEnabledMask, in Unity.Burst.Intrinsics.v128 chunkEnabledMask)
+    {
+        NativeArray<LocalTransform> transforms = chunk.GetNativeArray(ref TransformHandle);
+        NativeArray<MoveSpeed> speeds = chunk.GetNativeArray(ref SpeedHandle);
+        int count = chunk.Count;
+        int chunks = count / 4;
+        int remainder = count % 4;
+        for (int i = 0; i < chunks; i++)
+        {
+            int idx = i * 4;
+            float4 px = new float4(transforms[idx].Position.x, transforms[idx + 1].Position.x, transforms[idx + 2].Position.x, transforms[idx + 3].Position.x);
+            float4 py = new float4(transforms[idx].Position.y, transforms[idx + 1].Position.y, transforms[idx + 2].Position.y, transforms[idx + 3].Position.y);
+            float4 pz = new float4(transforms[idx].Position.z, transforms[idx + 1].Position.z, transforms[idx + 2].Position.z, transforms[idx + 3].Position.z);
+            float3 f0 = math.forward(transforms[idx].Rotation);
+            float3 f1 = math.forward(transforms[idx + 1].Rotation);
+            float3 f2 = math.forward(transforms[idx + 2].Rotation);
+            float3 f3 = math.forward(transforms[idx + 3].Rotation);
+
+            float4 fx = new float4(f0.x, f1.x, f2.x, f3.x);
+            float4 fy = new float4(f0.y, f1.y, f2.y, f3.y);
+            float4 fz = new float4(f0.z, f1.z, f2.z, f3.z);
+            float4 s = new float4(speeds[idx].Value, speeds[idx + 1].Value, speeds[idx + 2].Value, speeds[idx + 3].Value);
+            float4 velocityScale = dt * s;
+            px += velocityScale * fx;
+            py += velocityScale * fy;
+            pz += velocityScale * fz;
+            LocalTransform t0 = transforms[idx]; t0.Position = new float3(px.x, py.x, pz.x); transforms[idx] = t0;
+            LocalTransform t1 = transforms[idx + 1]; t1.Position = new float3(px.y, py.y, pz.y); transforms[idx + 1] = t1;
+            LocalTransform t2 = transforms[idx + 2]; t2.Position = new float3(px.z, py.z, pz.z); transforms[idx + 2] = t2;
+            LocalTransform t3 = transforms[idx + 3]; t3.Position = new float3(px.w, py.w, pz.w); transforms[idx + 3] = t3;
+        }
+        int remStart = chunks * 4;
+        for (int i = 0; i < remainder; i++)
+        {
+            int idx = remStart + i;
+            LocalTransform t = transforms[idx];
+            t.Position += dt * speeds[idx].Value * math.forward(t.Rotation);
+            transforms[idx] = t;
+        }
+    }
+}
+
+[BurstCompile(FloatPrecision = FloatPrecision.Medium, FloatMode = FloatMode.Fast)]
+[WithAll(typeof(MoveForward))]
+partial struct FixedPointMovementJob : IJobEntity
+{
+    public int dtRaw; // 16:16
+
+    void Execute(in MoveSpeed speed, ref LocalTransform transform)
+    {
+        const int SHIFT = 16;
+        const int ONE = 1 << SHIFT;
+        int speedRaw = (int)(speed.Value * ONE);
+        float3 fwd = math.forward(transform.Rotation);
+        int fwdXRaw = (int)(fwd.x * ONE);
+        int fwdYRaw = (int)(fwd.y * ONE);
+        int fwdZRaw = (int)(fwd.z * ONE);
+        int posXRaw = (int)(transform.Position.x * ONE);
+        int posYRaw = (int)(transform.Position.y * ONE);
+        int posZRaw = (int)(transform.Position.z * ONE);
+        long stepScale = ((long)dtRaw * speedRaw) >> SHIFT;
+        int deltaX = (int)((stepScale * fwdXRaw) >> SHIFT);
+        int deltaY = (int)((stepScale * fwdYRaw) >> SHIFT);
+        int deltaZ = (int)((stepScale * fwdZRaw) >> SHIFT);
+        posXRaw += deltaX;
+        posYRaw += deltaY;
+        posZRaw += deltaZ;
+
+        transform.Position = new float3((float)posXRaw / ONE, (float)posYRaw / ONE, (float)posZRaw / ONE);
     }
 }
 
